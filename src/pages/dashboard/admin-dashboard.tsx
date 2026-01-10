b4ead3ae626c1f807fe3ae0869ed2f501c682356
AmritBahadurShrestha <amritshrestha1100@gmail.com>
2026-01-10 20:33:14 +0545

    bar chart

--- src/pages/dashboard/admin-dashboard.tsx
+++ src/pages/dashboard/admin-dashboard.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { dashboard } from '../../api/dashboard.api';
 import CountCard from '../../components/dashboard/count-card';
-import GenderCharts from '../../components/dashboard/GenderChart';
+import GenderChart from '../../components/dashboard/GenderChart';
 
 interface DashboardData {
   students: number
@@ -60,7 +60,7 @@ const AdminDashboard = () => {
       </div>
 
       <div>
-        <GenderCharts/>
+        <GenderChart/>
       </div>
     </main>
   )
