576c826bd1d9021db961e516c65b8db634d0dff2
AmritBahadurShrestha <amritshrestha1100@gmail.com>
2026-01-10 15:17:02 +0545

    style search

--- src/pages/course/SearchInput.tsx
+++ src/pages/course/SearchInput.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 const SearchInput: React.FC<IProps> = ({ placeholder, id, tempInputValue,setTempInputValue }) => {
   return (
-    <div className="p-2">
+    <div>
       <input
       value={tempInputValue}
       onChange={(e)=>setTempInputValue(e.target.value)}
@@ -16,7 +16,7 @@ const SearchInput: React.FC<IProps> = ({ placeholder, id, tempInputValue,setTemp
         placeholder={placeholder}
         className="
           w-full sm:w-1/3
-          px-2 py-2
+          px-4 py-2
           text-sm text-gray-700
           bg-white
           border border-gray-300
